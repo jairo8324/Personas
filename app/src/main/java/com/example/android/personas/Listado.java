1a9727c1b0da7cfa1ec64cdaf790b38983d38fdb
“Francisco <jtorres8324@gmail.com>
2017-04-04 20:57:41 -0500

    Interfax Terminada

--- app/src/main/java/com/example/android/personas/Listado.java
+++ app/src/main/java/com/example/android/personas/Listado.java
@@ -3,18 +3,11 @@ package com.example.android.personas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Principal extends AppCompatActivity {
+public class Listado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
-
-
-
-
-
-
-
+        setContentView(R.layout.activity_listado);
     }
 }
